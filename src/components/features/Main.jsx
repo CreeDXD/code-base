79e9d108d8ea954b68425951124989f9ed714941
CreeDXD <pclio92@gmail.com>
2025-01-07 19:42:51 +0100

    add link to card

--- src/components/features/Main.jsx
+++ src/components/features/Main.jsx
@@ -5,9 +5,27 @@ export function Main() {
     <div className="container__main">
       <h1>Main</h1>
 
-      <Card title="Card title" description="Card description" imageUrl={logo} />
-      <Card title="Card title" description="Card description" imageUrl={logo} />
-      <Card title="Card title" description="Card description" imageUrl={logo} />
+      <a href="/page1">
+        <Card
+          title="Card title"
+          description="Card description"
+          imageUrl={logo}
+        />
+      </a>
+      <a href="/page2">
+        <Card
+          title="Card title"
+          description="Card description"
+          imageUrl={logo}
+        />
+      </a>
+      <a href="/page3">
+        <Card
+          title="Card title"
+          description="Card description"
+          imageUrl={logo}
+        />
+      </a>
     </div>
   )
 }
