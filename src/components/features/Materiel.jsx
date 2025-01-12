98fbdf814443d0f338693828fed358587d00441b
CreeDXD <pclio92@gmail.com>
2025-01-12 11:44:39 +0100

    modif style for section materal

--- src/components/features/Materiel.jsx
+++ src/components/features/Materiel.jsx
@@ -1,11 +1,9 @@
 const Materiel = () => {
   return (
-    <div className="container__Materiel">
-      <div className="call-to-action">
-        <h1>Welcome to Our Service</h1>
-        <p>Discover the best solutions for your needs.</p>
-        <button>Get Started</button>
-      </div>
+    <div className="container__materiel">
+      <h1>Welcome to Our Service</h1>
+      <p>Discover the best solutions for your needs.</p>
+      <button className="">Get Started</button>
     </div>
   )
 }
