50f4218db175b20a586d8e5d178fd5d2819475c1
CreeDXD <pclio92@gmail.com>
2025-01-16 23:51:42 +0100

    add contact for footer and style

--- src/components/features/Footer.jsx
+++ src/components/features/Footer.jsx
@@ -2,6 +2,12 @@ export function Footer() {
   return (
     <div className="container__footer">
       <h1>Footer</h1>
+      <div className="footer__contact">
+        <p>
+          Contact us at:{' '}
+          <a href="mailto:contact@example.com">contact@example.com</a>
+        </p>
+      </div>
     </div>
   )
 }
