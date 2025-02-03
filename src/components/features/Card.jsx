6186f2761b019d60f95bb557eba63ec29177beb0
CreeDXD <pclio92@gmail.com>
2025-02-03 22:43:55 +0100

    change function so anchor transition somothly and change style so card is responsive part1

--- src/components/features/Card.jsx
+++ src/components/features/Card.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
 const Card = ({ title, description, imageUrl, link }) => {
+  function smoothScroll() {
+    document.getElementById(link).scrollIntoView({ behavior: 'smooth' })
+  }
   return (
-    <a href={link} className="card__link">
-      <div className="card" >
+    
+      <div className="card" onClick={smoothScroll} >
         <img src={imageUrl} alt={title} className="card__image" />
         <div className="card__content">
           <h2 className="card__title">{title}</h2>
           <p className="card__description">{description}</p>
         </div>
-        <button className="">Get Started</button>
+        
+        <button  className="">Get Started</button>
       </div>
-    </a>
   )
 }
 
--- src/components/features/Main.jsx
+++ src/components/features/Main.jsx
@@ -1,36 +1,40 @@
 import Card from './Card'
-import Carrousel from '../shared/caroussel/Carrousel'
+// import Carrousel from '../shared/caroussel/Carrousel'
 import { carrouselData } from '../../data/data'
 import Materiel from '../features/Materiel'
 import Cours from '../features/Cours'
 import Reparation from '../features/Reparation'
-
 export function Main() {
-  console.log(document.getElementById('materiel'))
+
+  
+
   return (
-    <div id='home' className="container__main">
+    <div id='home' className="container__main" >
       {/* <h1>Main</h1> */}
 
       <Card
         title="Matériel"
         description={carrouselData[0].altText}
         imageUrl={carrouselData[0].imageUrl}
-        link="#materiel"
+        link="materiel"
+       
       />
       <Card
         title="Cours"
         description={carrouselData[1].altText}
         imageUrl={carrouselData[1].imageUrl}
-        link="#cours"
+        link="cours"
       />
       <Card
         title="Réparation"
         description={carrouselData[2].altText}
         imageUrl={carrouselData[2].imageUrl}
-        link="#reparation"
+        link="reparation"
       />
 
-      <Materiel />
+      <Materiel 
+      
+      />
       <Cours />
       <Reparation />
       {/* <Carrousel images={carouselData} /> */}
