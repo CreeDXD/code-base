8d929a470b947e396bad33031378875c41a065a3
CreeDXD <pclio92@gmail.com>
2025-02-05 00:00:11 +0100

    card style responsive part2

--- src/components/features/Main.jsx
+++ src/components/features/Main.jsx
@@ -5,36 +5,31 @@ import Materiel from '../features/Materiel'
 import Cours from '../features/Cours'
 import Reparation from '../features/Reparation'
 export function Main() {
-
-  
-
   return (
-    <div id='home' className="container__main" >
+    <div id="home" className="container__main">
       {/* <h1>Main</h1> */}
+      <div className="cards">
+        <Card
+          title="Matériel"
+          description={carrouselData[0].altText}
+          imageUrl={carrouselData[0].imageUrl}
+          link="materiel"
+        />
+        <Card
+          title="Cours"
+          description={carrouselData[1].altText}
+          imageUrl={carrouselData[1].imageUrl}
+          link="cours"
+        />
+        <Card
+          title="Réparation"
+          description={carrouselData[2].altText}
+          imageUrl={carrouselData[2].imageUrl}
+          link="reparation"
+        />
+      </div>
 
-      <Card
-        title="Matériel"
-        description={carrouselData[0].altText}
-        imageUrl={carrouselData[0].imageUrl}
-        link="materiel"
-       
-      />
-      <Card
-        title="Cours"
-        description={carrouselData[1].altText}
-        imageUrl={carrouselData[1].imageUrl}
-        link="cours"
-      />
-      <Card
-        title="Réparation"
-        description={carrouselData[2].altText}
-        imageUrl={carrouselData[2].imageUrl}
-        link="reparation"
-      />
-
-      <Materiel 
-      
-      />
+      <Materiel />
       <Cours />
       <Reparation />
       {/* <Carrousel images={carouselData} /> */}
