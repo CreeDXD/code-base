66851bf076cf000a8f05cad64c40ab1a405cd08d
CreeDXD <pclio92@gmail.com>
2025-02-06 19:12:20 +0100

    remove link from header and add smoothtransition to header list

--- src/components/features/Header.jsx
+++ src/components/features/Header.jsx
@@ -1,24 +1,17 @@
 import BurgerSection from '../features/Burger_Section'
 
 export function Header() {
+  const smoothScroll = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' })
+  }
   return (
-    <div className="container__header">
+    <div className="container__header" id="hoime">
       <nav>
         <ul>
-          <li>
-            <a href="#home" className="logo_name">
-              JCN Club Informatique
-            </a>
-          </li>
-          <li>
-            <a href="#materiel">Matériel</a>
-          </li>
-          <li>
-            <a href="#cours">Cours</a>
-          </li>
-          <li>
-            <a href="#reparation"> Réparation</a>
-          </li>
+          <li onClick={() => smoothScroll('home')}>JCN Club Informatique</li>
+          <li onClick={() => smoothScroll('materiel')}>Matériel</li>
+          <li onClick={() => smoothScroll('cours')}>Cours</li>
+          <li onClick={() => smoothScroll('reparation')}>Réparation</li>
           <li>
             <BurgerSection />
           </li>
